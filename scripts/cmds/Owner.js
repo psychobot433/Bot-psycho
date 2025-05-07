c5cf9fc1f52df01fd76df4a16e6328754c056a78
psychobot433 <botp4959@gmail.com>
2025-05-07 17:15:42 +0600

    Update Owner.js

--- scripts/cmds/Owner.js
+++ scripts/cmds/Owner.js
@@ -32,7 +32,7 @@ module.exports = {
       'timeZone': "Asia/Dhaka",
       'hour12': true
     });
-    const _0x3b754f = "╔╝❮" + _0x8f4cd + "❯╚╗\n━━━━━━━━━━━━━━━━━━━━━━\n𝐍𝐀𝐌𝐄: 𝐌𝐀𝐇𝐀𝐁𝐔𝐁 𝐑𝐀𝐇𝐌𝐀𝐍\n𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍: 𝐈𝐒𝐋𝐀𝐌\n𝐀𝐃𝐃𝐑𝐄𝐒𝐒: 𝐁𝐎𝐆𝐔𝐑𝐀,𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇\n𝐆𝐄𝐍𝐃𝐄𝐑: 𝐌𝐀𝐋𝐄\n𝐀𝐆𝐄: 18\n𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏: 𝐅𝐀𝐈𝐋𝐃\n𝐖𝐎𝐑𝐊: 𝐆𝐑𝐎𝐂𝐄𝐑𝐘 𝐒𝐇𝐎𝐏\n𝐆𝐌𝐀𝐈𝐋: mahaburpk479@gmail.com\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://m.me/www.xnxx.com.140 \n𝐌𝐀𝐒𝐒𝐄𝐍𝐆𝐄𝐑: m.me/100014754734049\n𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+8801613356376\n𝐈𝐌𝐎: 𝐏𝐄𝐑𝐒𝐎𝐍𝐀𝐋🫣\n𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: t.me/Mahabub_x_Rahman\n━━━━━━━━━━━━━━━━━━━━━━\n\nBot Prefix: ( . )\nBot Name: MAHABUB BOT\ngc Name: " + _0x47034c + "\nTime:【 " + _0x27ce82 + " || " + _0x3ac793 + " \n━━━━━━━━━━━━━━━━━━━━━━";
+    const _0x3b754f = "╔╝❮" + _0x8f4cd + "❯╚╗\n━━━━━━━━━━━━━━━━━━━━━━\n𝐍𝐀𝐌𝐄: 🆂🅾🆄🆁🅰🆅\n𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍: SONATON\n𝐀𝐃𝐃𝐑𝐄𝐒𝐒: SATKANIYA,CHITTAGONG,𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇\n𝐆𝐄𝐍𝐃𝐄𝐑: 𝐌𝐀𝐋𝐄\n𝐀𝐆𝐄: 20\n𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏: YES🤭\n𝐖𝐎𝐑𝐊: FARMCHIST\n𝐆𝐌𝐀𝐈𝐋: sourabnath991@gmail.com\n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊: https://www.facebook.com/share/12LAVUaKSqP/ \n𝐌𝐀𝐒𝐒𝐄𝐍𝐆𝐄𝐑: https://m.me/s.o.u.r.a.v.tsu.941375\n𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏: wa.me/+8801880962478\n𝐈𝐌𝐎: ❌❌\n𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌: ❌❌\n━━━━━━━━━━━━━━━━━━━━━━\n\nBot Prefix: ( . )\nBot Name: 🆂🅾🆄🆁🅰🆅 BOT\ngc Name: " + _0x47034c + "\nTime:【 " + _0x27ce82 + " || " + _0x3ac793 + " \n━━━━━━━━━━━━━━━━━━━━━━";
     _0x3b0c92.reply({
       'body': _0x3b754f,
       'attachment': await global.utils.getStreamFromURL("https://graph.facebook.com/100014754734049/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662")
